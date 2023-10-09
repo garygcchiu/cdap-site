ee48a97a8fe5943b22d036e4726a1160dbe77a08
Gary Chiu <gary.chiu@datadoghq.com>
2023-10-09 17:03:33 -0400

    fade images

--- src/components/ui/TabbedSlideshow.tsx
+++ src/components/ui/TabbedSlideshow.tsx
@@ -1,7 +1,7 @@
 'use client';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import clsx from 'clsx';
-import Image, { StaticImageData } from 'next/image';
+import { StaticImageData } from 'next/image';
 import NextImage from '@/components/ui/NextImage';
 
 export interface TabProps {
@@ -17,6 +17,21 @@ export interface TabbedSlideshowProps {
 
 export const TabbedSlideshow = ({ data }: TabbedSlideshowProps) => {
     const [selectedTab, setSelectedTab] = useState(0);
+    const [fadingImageIndex, setFadingImageIndex] = useState(0);
+    const [fadeOpacity, setFadeOpacity] = useState(1);
+
+    useEffect(() => {
+        // Start the fade-out effect
+        setFadeOpacity(0);
+
+        const timeout = setTimeout(() => {
+            // After the transition completes, set the fading image to the current one
+            setFadingImageIndex(selectedTab);
+            // Reset the opacity for the next fade-out
+            setFadeOpacity(1);
+        }, 300);
+        return () => clearTimeout(timeout);
+    }, [selectedTab]);
 
     return (
         <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
@@ -36,8 +51,20 @@ export const TabbedSlideshow = ({ data }: TabbedSlideshowProps) => {
                 ))}
             </div>
             <div className="col-span-2 flex flex-col space-y-16 mt-8 md:mt-0">
-                <div className={'flex justify-center max-w-lg self-center'}>
+                <div className={'flex justify-center max-w-lg self-center relative'}>
+                    {/* Back Layer (always visible) */}
                     <NextImage src={data[selectedTab].image} alt={'image'} useSkeleton={true} />
+                    {/* Front Layer (fades out) */}
+                    <div
+                        className={'absolute top-0 left-0 transition-opacity duration-300'}
+                        style={{ opacity: fadeOpacity }}
+                    >
+                        <NextImage
+                            src={data[fadingImageIndex].image}
+                            alt={'image'}
+                            useSkeleton={true}
+                        />
+                    </div>
                 </div>
                 <p className={'text-foreground'}>{data[selectedTab].body}</p>
             </div>
